578abf13ac1511ae5adb49b93a9d642a7af3d46c
Steffen Tröster <steffen.troester@googlemail.com>
2014-09-15 20:42:02 +0200

    update design

--- src/main/java/de/stetro/feedpaper/MainActivity.java
+++ src/main/java/de/stetro/feedpaper/MainActivity.java
@@ -1,11 +1,9 @@
 package de.stetro.feedpaper;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import de.stetro.feedpaper.util.FeedLoaderAsyncTask;
@@ -16,7 +14,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         setupLayoutCallbacks();
     }
@@ -27,24 +25,8 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 FeedLoaderAsyncTask task = new FeedLoaderAsyncTask(getApplicationContext());
-                task.execute("Astro_Alex");
+                task.execute("BestEarthPix");
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
